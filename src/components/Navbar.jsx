fd55bb98d4632d92b76032cd01cf6ddc21f39edf
Vishal Sharma <vishals200415@gmail.com>
2023-06-07 23:04:31 +0530

    !!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,39 @@ import styled from 'styled-components'
 
 const Container = styled.div`
     height: 60px;
-    background-color: black;
- `
+`
+const Wrapper = styled.div`
+    padding: 10px 20px;
+    display: flex;
+    justify-content: space-between;
+`
+
+const Left = styled.div`
+    flex: 1;
+`
+const Language = styled.span`
+    font-size: 14px;
+    cursor: pointer;
+`
+
+const Center = styled.div`
+    flex: 1;
+`
+const Right = styled.div`
+    flex: 1;
+`
 
 export default function Navbar() {
   return (
     <Container>
-        Navbar
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          {/* <SearchContainer></SearchContainer> */}
+        </Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
     </Container>
   )
 }
